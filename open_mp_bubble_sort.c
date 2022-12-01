8dc7384b6ca336011d9aefa9969dc826b4478016
Victor Fernandes <victor.faria@olxbr.com>
2022-12-01 00:31:43 -0300

    fixing algorithm

--- open_mp_bubble_sort.c
+++ open_mp_bubble_sort.c
@@ -22,7 +22,7 @@ void oddEvenSort(int arr[], int n)
         // Perform Bubble sort on odd indexed element
         for (int i = 1; i <= n - 2; i = i + 2) {
             if (arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
+                swap(&arr[i], &arr[i + 1]);
                 isSorted = 0;
             }
         }
@@ -30,11 +30,22 @@ void oddEvenSort(int arr[], int n)
         // Perform Bubble sort on even indexed element
         for (int i = 0; i <= n - 2; i = i + 2) {
             if (arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
+                swap(&arr[i], &arr[i + 1]);
                 isSorted = 0;
             }
         }
     }
  
     return;
+}
+
+int main(){
+    int N = N;
+    int a[N] = {6, 10, 3,2,1,4,5,};
+
+    oddEvenSort(a, N);
+
+    for (int i = 0; i < N; i++){
+        printf("%d \n", a[i]);
+    }
 }
