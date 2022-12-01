6981c72df5e39ae0518c9715b245c8d6304c7a96
Victor Fernandes <victor.faria@olxbr.com>
2022-12-01 00:24:16 -0300

    algorithm

---
+++ open_mp_bubble_sort.c
@@ -0,0 +1,40 @@
+#include <omp.h>
+#include <stdio.h>
+
+/* swaps the elements */
+void swap(int * x, int * y)
+{
+    int temp;
+ 
+    temp = *x;
+    *x = *y;
+    *y = temp; 
+}
+
+// A function to sort the algorithm using Odd Even sort
+void oddEvenSort(int arr[], int n)
+{
+    int isSorted = 0; // Initially array is unsorted
+ 
+    while (!isSorted) {
+        isSorted = 1;
+ 
+        // Perform Bubble sort on odd indexed element
+        for (int i = 1; i <= n - 2; i = i + 2) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                isSorted = 0;
+            }
+        }
+ 
+        // Perform Bubble sort on even indexed element
+        for (int i = 0; i <= n - 2; i = i + 2) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                isSorted = 0;
+            }
+        }
+    }
+ 
+    return;
+}
